2a27cd40f61b313db84ae2d815f7e5725a8e1624
MichaelFonsec <jacquelinemartins743@gmail.com>
2023-04-19 05:36:39 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,9 @@
+alert("oi");
+        var nomes = ["Thamires", "Michael", "Paulo", "José"];
+
+        function pushvetor() {
+               
+
+               alert(nomes);
+               var lista = document.getElementById(h1);
+        }
